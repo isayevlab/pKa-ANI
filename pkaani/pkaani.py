01d5b60a7c15ae862ddcfff321fc0bafb1c98e9f
Nigel W. Moriarty <nwmoriarty@lbl.gov>
2022-04-28 15:54:51 -0700

    slightly better results object

--- pkaani/pkaani.py
+++ pkaani/pkaani.py
@@ -45,7 +45,7 @@ def calculate_pka(pdbfiles):
     for fpdb in pdbfiles:
         print('Calculating pKa for %s' % fpdb)    
 
-        pkadict[fpdb]=[]
+        pkadict[fpdb]={}
 
         basename=fpdb.rsplit('.', 1)[0]
         infile=str(basename)+".pdb"
@@ -128,7 +128,7 @@ def calculate_pka(pdbfiles):
                
                lres=str(res[index[0]].strip())
                lchid=str(chainid[index[0]]).strip()
-               lrnum=str(res_no[index[0]])
+               lrnum=(chainid[index[0]].strip(), res_no[index[0]].strip())
     
                mychain=lchid
                if not lchid: mychain='A'
@@ -174,12 +174,10 @@ def calculate_pka(pdbfiles):
       
                estimate_pka=model.predict(X)
     
-               wres=lres+"-"+lrnum
+               wres=lres+"-"+str(lrnum)
                writer.writerow([wres,mychain,'{:2.2f}'.format(estimate_pka[0])])
 
-               resdict={}
-               resdict[lrnum]=lres,estimate_pka[0]
-               pkadict[fpdb].append(resdict)    
+               pkadict[fpdb][lrnum]=lres,estimate_pka[0]
         
         fo.close()                  
 
